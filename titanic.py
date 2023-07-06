7db662b02ada421b7595fc57bbf3d1ae064cb6fc
gnanadeepika333 <96773023+gnanadeepika333@users.noreply.github.com>
2023-07-06 18:12:36 +0530

    Add files via upload

---
+++ titanic.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# linear algebra
+import numpy as np 
+
+# data processing
+import pandas as pd 
+
+# data visualization
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+from matplotlib import pyplot as plt
+from matplotlib import style
+
+# Algorithms
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
+
+
+# In[5]:
+
+
+test_df = pd.read_csv("test.csv")
+train_df = pd.read_csv("train.csv")
+
+
+# In[6]:
+
+
+train_df.info()
+
+
+# In[10]:
+
+
+
+train_df.describe()
+
+
+# In[11]:
+
+
+train_df.head(8)
+
+
+# In[12]:
+
+
+total = train_df.isnull().sum().sort_values(ascending=False)
+percent_1 = train_df.isnull().sum()/train_df.isnull().count()*100
+percent_2 = (round(percent_1, 1)).sort_values(ascending=False)
+missing_data = pd.concat([total, percent_2], axis=1, keys=['Total', '%'])
+missing_data.head(5)
+
+
+# In[13]:
+
+
+train_df.columns.values
+
+
+# In[14]:
+
+
+survived = 'survived'
+not_survived = 'not survived'
+fig, axes = plt.subplots(nrows=1, ncols=2,figsize=(10, 4))
+women = train_df[train_df['Sex']=='female']
+men = train_df[train_df['Sex']=='male']
+ax = sns.distplot(women[women['Survived']==1].Age.dropna(), bins=18, label = survived, ax = axes[0], kde =False)
+ax = sns.distplot(women[women['Survived']==0].Age.dropna(), bins=40, label = not_survived, ax = axes[0], kde =False)
+ax.legend()
+ax.set_title('Female')
+ax = sns.distplot(men[men['Survived']==1].Age.dropna(), bins=18, label = survived, ax = axes[1], kde = False)
+ax = sns.distplot(men[men['Survived']==0].Age.dropna(), bins=40, label = not_survived, ax = axes[1], kde = False)
+ax.legend()
+_ = ax.set_title('Male')
+
+
+# In[15]:
+
+
+FacetGrid = sns.FacetGrid(train_df, row='Embarked', size=4.5, aspect=1.6)
+FacetGrid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette=None,  order=None, hue_order=None )
+FacetGrid.add_legend()
+
+
+# In[16]:
+
+
+sns.barplot(x='Pclass', y='Survived', data=train_df)
+
+
+# In[17]:
+
+
+grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+grid.add_legend();
+
+
+# In[18]:
+
+
+data = [train_df, test_df]
+for dataset in data:
+    dataset['relatives'] = dataset['SibSp'] + dataset['Parch']
+    dataset.loc[dataset['relatives'] > 0, 'not_alone'] = 0
+    dataset.loc[dataset['relatives'] == 0, 'not_alone'] = 1
+    dataset['not_alone'] = dataset['not_alone'].astype(int)
+train_df['not_alone'].value_counts()
+
+
+# In[19]:
+
+
+axes = sns.factorplot('relatives','Survived', 
+                      data=train_df, aspect = 2.5, )
+
+
+# In[ ]:
+
+
+
+
